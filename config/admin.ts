00b395b063b785091216d8f313d80999fd17015f
kienbui195 <kaybi9x@gmail.com>
2024-08-28 12:01:02 +0700

    them file cicd

--- config/admin.ts
+++ config/admin.ts
@@ -1,6 +1,9 @@
 export default ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
+    options: {
+      expiresIn: '300s'
+    }
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
